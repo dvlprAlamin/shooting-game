393a8af2ad613630bb6f64c0bbff4e58020660ad
dvlpralamin <alaminhowladerofficial@gmail.com>
2024-08-02 21:29:42 +0600

    duplication issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,166 +1,78 @@
-// import * as TWEEN from '@tweenjs/tween.js';
-// import { PointerLockControls, Sky } from '@react-three/drei';
-// import { Ground } from '@/Ground.jsx';
-// import { CapsuleCollider, Physics, RigidBody } from '@react-three/rapier';
-// import { Player } from '@/Player.jsx';
-// import { Canvas, useFrame } from '@react-three/fiber';
-// import { usePointerLockControlsStore } from './store/PointerLockControlStore';
-// import { usePlayerStore } from './store/PlayersStore';
-// import { SocketManager } from './components/SocketManager';
-// import { Fragment } from 'react';
-
-// const shadowOffset = 50;
-
-// export const App = () => {
-//   const { players } = usePlayerStore();
-
-//   console.log('players', players);
-//   return (
-//     <>
-//       <SocketManager />
-//       <Canvas camera={{ fov: 45 }} shadows>
-//         <Scene />
-//       </Canvas>
-//     </>
-//   );
-// };
-
-// export default App;
-
-// const Scene = () => {
-//   const { players, currentPlayer } = usePlayerStore();
-//   console.log('players', players);
-
-//   useFrame(() => {
-//     TWEEN.update();
-//   });
-
-//   const pointerLockControlsLockHandler = () => {
-//     usePointerLockControlsStore.setState({ isLock: true });
-//   };
-
-//   const pointerLockControlsUnlockHandler = () => {
-//     usePointerLockControlsStore.setState({ isLock: false });
-//   };
-//   return (
-//     <>
-//       <PointerLockControls
-//         onLock={pointerLockControlsLockHandler}
-//         onUnlock={pointerLockControlsUnlockHandler}
-//       />
-//       <Sky sunPosition={[100, 20, 100]} />
-//       <ambientLight intensity={1.5} />
-//       <directionalLight
-//         castShadow
-//         intensity={1.5}
-//         shadow-mapSize={4096}
-//         shadow-camera-top={shadowOffset}
-//         shadow-camera-bottom={-shadowOffset}
-//         shadow-camera-left={shadowOffset}
-//         shadow-camera-right={-shadowOffset}
-//         position={[100, 100, 0]}
-//       />
-//       <Physics gravity={[0, -20, 0]}>
-//         <Ground />
-//         <Player />
-//         {Object.keys(players)?.map((id) => (
-//           <Fragment key={id}>
-//             {currentPlayer.id !== id ? (
-//               <RigidBody
-//                 position={players[id].position}
-//                 colliders={false}
-//                 mass={1}
-//                 lockRotations
-//               >
-//                 <mesh position={players[id].position} castShadow>
-//                   <capsuleGeometry args={[0.5, 0.5]} />
-//                   <meshBasicMaterial color={'yellow'} />
-//                   <CapsuleCollider args={[0.75, 0.5]} />
-//                 </mesh>
-//               </RigidBody>
-//             ) : (
-//               <></>
-//             )}
-//           </Fragment>
-//         ))}
-//       </Physics>
-//     </>
-//   );
-// };
-import * as TWEEN from '@tweenjs/tween.js';
+import React, { useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { PointerLockControls, Sky } from '@react-three/drei';
-import { Ground } from './Ground';
+import { Ground } from '@/Ground.jsx';
 import { Physics } from '@react-three/rapier';
-import { Player } from './Player';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { useEffect, useState } from 'react';
+import { Player } from '@/Player.jsx';
 import { io } from 'socket.io-client';
-import { usePointerLockControlsStore } from './store/PointerLockControlStore';
 
-const socket = io('http://localhost:4000');
+export const socket = io('http://localhost:3000');
 
-export const App = () => {
+const App = () => {
   const [players, setPlayers] = useState({});
-  console.log('players', players);
 
   useEffect(() => {
-    socket.on('existingPlayers', (existingPlayers) => {
-      setPlayers(existingPlayers);
+    // Handle current players
+    socket.on('currentPlayers', (currentPlayers) => {
+      console.log('Current Players:', currentPlayers);
+      setPlayers(currentPlayers);
     });
 
+    // Handle new player
     socket.on('newPlayer', (newPlayer) => {
-      setPlayers((prev) => ({ ...prev, [newPlayer.id]: newPlayer }));
+      console.log('New Player:', newPlayer);
+      setPlayers((prevPlayers) => ({
+        ...prevPlayers,
+        [newPlayer.id]: {
+          position: newPlayer.position,
+          rotation: newPlayer.rotation,
+        },
+      }));
     });
 
-    socket.on('move', (player) => {
-      setPlayers((prev) => ({ ...prev, [player.id]: player }));
+    // Handle player movement
+    socket.on('playerMoved', (player) => {
+      console.log('Player Moved:', player);
+      setPlayers((prevPlayers) => ({
+        ...prevPlayers,
+        [player.id]: {
+          position: player.position,
+          rotation: player.rotation,
+        },
+      }));
     });
 
+    // Handle player disconnection
     socket.on('playerDisconnected', (id) => {
-      setPlayers((prev) => {
-        const previousValue = { ...prev };
-        delete previousValue[id];
-        // const { [id]: _, ...rest } = prev;
-        return previousValue;
+      console.log('Player Disconnected:', id);
+      setPlayers((prevPlayers) => {
+        const newPlayers = { ...prevPlayers };
+        delete newPlayers[id];
+        return newPlayers;
       });
     });
 
     return () => {
-      socket.off('existingPlayers');
+      socket.off('currentPlayers');
       socket.off('newPlayer');
-      socket.off('move');
+      socket.off('playerMoved');
       socket.off('playerDisconnected');
     };
   }, []);
 
   return (
-    <>
-      <Canvas camera={{ fov: 45 }} shadows>
-        <Scene players={players} />
-      </Canvas>
-    </>
+    <Canvas camera={{ fov: 45 }} shadows>
+      <Scene players={players} />
+    </Canvas>
   );
 };
 
+export default App;
+
 const Scene = ({ players }) => {
-  useFrame(() => {
-    TWEEN.update();
-  });
-
-  const pointerLockControlsLockHandler = () => {
-    usePointerLockControlsStore.setState({ isLock: true });
-  };
-
-  const pointerLockControlsUnlockHandler = () => {
-    usePointerLockControlsStore.setState({ isLock: false });
-  };
-
   return (
     <>
-      <PointerLockControls
-        onLock={pointerLockControlsLockHandler}
-        onUnlock={pointerLockControlsUnlockHandler}
-      />
+      <PointerLockControls />
       <Sky sunPosition={[100, 20, 100]} />
       <ambientLight intensity={1.5} />
       <directionalLight
@@ -187,5 +99,3 @@ const Scene = ({ players }) => {
     </>
   );
 };
-
-export default App;
